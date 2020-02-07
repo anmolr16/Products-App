2cc0aea0952390dd01427c318a3c61aa0379f48f
anmol <anmolr@kpit.com>
2020-02-07 16:11:42 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,100 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+import {MatTableDataSource} from '@angular/material/table';
+import {SelectionModel} from '@angular/cdk/collections';
+
+export interface ProductElement {
+  id: number;
+  name: string;
+  category: string;
+}
+
+let ELEMENT_DATA: ProductElement[] = [
+  { id: 1, name: 'Sandwich', category: 'Food' },
+  { id: 2, name: 'Nachos', category: 'Food' },
+  { id: 3, name: 'Burger', category: 'Food' },
+  { id: 4, name: 'Fries', category: 'Food' },
+  { id: 5, name: 'Noodles', category: 'Food' },
+  { id: 6, name: 'Soft Drink', category: 'Drinks' },
+  { id: 7, name: 'Lemonade', category: 'Drinks' },
+  { id: 8, name: 'Mojito', category: 'Drinks' },
+  { id: 9, name: 'Ice Tea', category: 'Drinks' },
+  { id: 10, name: 'Juice', category: 'Drinks' }
+];
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'product';
+  displayedColumns: string[] = ['select', 'id', 'name', 'category'];
+  dataSource = new MatTableDataSource<ProductElement>(ELEMENT_DATA);
+  selection = new SelectionModel<ProductElement>(true, []);
+  cartLength: number;
+  myForm: FormGroup;
+  backCart = false;
+  addCart  = true;
+
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected() ?
+        this.selection.clear() :
+        this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: ProductElement): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.id + 1}`;
+  }
+
+  showCart() {
+    this.addCart = false;
+    this.backCart = true;
+    this.displayedColumns = ['id', 'name', 'category'];
+    this.dataSource = new MatTableDataSource<ProductElement>(this.selection.selected);
+  }
+
+  backToCart() {
+    this.addCart = true;
+    this.backCart = false;
+    this.selection.clear();
+    this.cartLength = null;
+    this.displayedColumns = ['select', 'id', 'name', 'category'];
+    this.dataSource = new MatTableDataSource<ProductElement>(ELEMENT_DATA);
+  }
+
+
+  addToCart(): void {
+    console.log(this.selection);
+    console.log(this.selection.selected.length);
+    if (this.selection.selected.length > 0 ) {
+      this.cartLength = this.selection.selected.length;
+    }
+  }
+
+  ngOnInit() {
+    this.myForm = new FormGroup({
+      name: new FormControl(''),
+      category: new FormControl('')
+    });
+  }
+
+  onSubmit(form: FormGroup) {
+    ELEMENT_DATA.push({id: ELEMENT_DATA.length + 1,  name: form.value.name, category: form.value.category});
+    this.dataSource = new MatTableDataSource<ProductElement>(ELEMENT_DATA);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,86 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { FormsModule } from '@angular/forms';
+import {MatBadgeModule} from '@angular/material/badge';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      MatButtonModule,
+      MatMenuModule,
+      MatToolbarModule,
+      MatIconModule,
+      MatCardModule,
+      MatSidenavModule,
+      MatTableModule,
+      MatSortModule,
+      MatCheckboxModule,
+      MatInputModule,
+      MatFormFieldModule,
+      MatSnackBarModule,
+      MatSelectModule,
+      MatExpansionModule,
+      MatTabsModule,
+      MatProgressSpinnerModule,
+      MatDialogModule,
+      MatButtonToggleModule,
+      MatPaginatorModule,
+      MatSlideToggleModule,
+      MatTooltipModule,
+      MatStepperModule,
+      MatListModule,
+      MatAutocompleteModule,
+      MatRadioModule,
+      MatDividerModule,
+      MatChipsModule,
+      MatDatepickerModule,
+      MatNativeDateModule,
+      MatProgressBarModule,
+    BrowserAnimationsModule,
+    MatBadgeModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
